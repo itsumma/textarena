238e8eae577c558f8bfb8ad32cd7d2ff34c623a6
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2020-12-25 19:06:21 +0800

    🏞  Image processor logic reduction

--- src/creators/image.ts
+++ src/creators/image.ts
@@ -2,27 +2,8 @@ import CreatorContext from 'interfaces/CreatorContext';
 import { insertImage } from 'utils';
 
 export default function image(context: CreatorContext): void {
-  const { focusElement } = context;
   let url = 'http://';
-  if (focusElement && focusElement.tagName === 'IMG') {
-    url = focusElement.getAttribute('src') || '';
-  }
-  if (focusElement?.children) {
-    for (let i = 0; i < focusElement.children.length; i += 1) {
-      const c = focusElement.children.item(i);
-      if (c?.tagName === 'IMG') {
-        url = c.getAttribute('src') || '';
-        break;
-      }
-    }
-  }
   const value = prompt('Введите IMAGE URL', url);
-  if (value === null) return;
-  if (focusElement && focusElement.tagName === 'IMG') {
-    if (value) {
-      focusElement.setAttribute('src', value);
-    }
-  } else if (value) {
-    insertImage(value, context);
-  }
+  if (value === null || !value) return;
+  insertImage(value, context);
 }
