bf6b91158c2a91d5a33d59060af5e28a53e8eebb
exsmund <exsmund@gmail.com>
2021-03-18 09:40:36 +0800

    Improve deployment

--- public/index.js
+++ public/index.js
@@ -18,69 +18,50 @@
 
   if (elem && (typeof Textarena !== 'undefined')) {
     let dataHtml;
-    // const storedData = localStorage.getItem('dataHtml');
-    // try {
-    //   if (storedData) {
-    //     dataHtml = JSON.parse(storedData);
-    //   }
-    // } catch {}
+    const storedData = localStorage.getItem('dataHtml');
+    try {
+      if (storedData) {
+        data = JSON.parse(storedData);
+        if (data && data.dataHtml && data.time && data.time > (+ new Date() - 60*60*24)) {
+          dataHtml = data.dataHtml;
+        }
+      }
+    } catch {}
     if (!dataHtml) {
-      dataHtml = `<h2>Простой текстовый редактор</h2>
+      dataHtml = `<h2>Простой визуальный редактор</h2>
         <p><a href="https://github.com/devopsprodigy/textarena/">Textarena</a> адаптирована для быстрой работы. Все функции доступны с помощью горячих клавиш.</p>
-`;
-        // content: `<h2>Простой визуальный редактор</h2>
-        // <p>Textarena адаптирована для быстрой работы. Все функции доступны с помощью горячих клавиш.</p>
-        // <arena-callout>
-        //   <p slot="title">
-        //     Внимание!
-        //   </p>
-        //   <arena-callout-body slot="body">
-        //     <ul>
-        //     <li>
-        //     Это удобно
-        //     </li><li>
-        //     Это быстро
-        //     </li>
-        //     </ul>
-        //   </arena-callout-body>
-        // </arena-callout>
-        // <ul>
-        // <li>Быстро.</li>
-        // <li>Удобно.</li>
-        // <li>Настраиваемо.</li>
-        // </ul>
-        // <figure><img src="https://www.itsumma.ru/img/services/musthave.png"></figure>
-        // <p>Выделите текст, появится панель для форматирования.</p>
-        // <hr />
-        // <p>Зажмите Ctrl (⌘ для Mac) или Alt (⌥ для Mac) и вы увидите подсказки.</p>
-        // <p>Ctrl (⌘) + B — изменит <b>жирность</b> выделенного текста.</p>
-        // <p>Ctrl (⌘) + I — выделенный текст станет <i>италиком</i>. Повторное нажатие уберёт италик.</p>
-        // <p>И так далее…</p>
-        // <p>Alt (⌥) + O — переключает между обычным текстом и нумерованным списком.</p>
-        // <h2>Легко доступные стили оформления текста</h2><h3>Заголовки</h3>
-        // <ol>
-        // <li>H2 — второй по величине.</li>
-        // <li>H3 — третий по величине.</li>
-        // <li>H4 — четвёртый.</li>
-        // </ol>
-        // <p>Почему нет первого? Во-первых, какие заголовки доступны в редакторе, настраивается. Во-вторых, не рекомендуется на странице использовать более одного заголовка первого уровня. На этой странице уже есть — «TEXTARENA» — в верху страницы.</p>`,
-// content: `
-// <h2>&nbsp;2</h2>
-// <p> а
-// на<b> берегу
-
-//     </b><i> на </i> берегу<!----></p>
-//     `,
-// content: `
-// <p>1<b>234</b>56789123</p>
-//     `,
+        <ul>
+        <li>Быстро.</li>
+        <li>Удобно.</li>
+        <li>Настраиваемо.</li>
+        </ul>
+        <figure><img src="https://www.itsumma.ru/img/services/musthave.png"></figure>
+        <p>Выделите текст, появится панель для форматирования.</p>
+        <hr />
+        <p>Зажмите Ctrl (⌘ для Mac) или Alt (⌥ для Mac) и вы увидите подсказки.</p>
+        <p>Ctrl (⌘) + B — изменит <b>жирность</b> выделенного текста.</p>
+        <p>Ctrl (⌘) + I — выделенный текст станет <i>италиком</i>. Повторное нажатие уберёт италик.</p>
+        <p>И так далее…</p>
+        <p>Alt (⌥) + O — переключает между обычным текстом и нумерованным списком.</p>
+        <h2>Легко доступные стили оформления текста</h2><h3>Заголовки</h3>
+        <ol>
+        <li>H2 — второй по величине.</li>
+        <li>H3 — третий по величине.</li>
+        <li>H4 — четвёртый.</li>
+        </ol>
+        <p>Почему нет первого? Во-первых, какие заголовки доступны в редакторе, настраивается. Во-вторых, не рекомендуется на странице использовать более одного заголовка первого уровня. На этой странице уже есть — «TEXTARENA» — в верху страницы.</p>`;
     }
     const initData = {
       dataHtml,
     }
     const htmlElem = document.getElementById('html');
     const onChange = debounce((data) => {
-      localStorage.setItem('dataHtml', JSON.stringify(data.dataHtml));
+      localStorage.setItem('dataHtml', JSON.stringify(
+        {
+          dataHtml: data.dataHtml,
+          time: + new Date(),
+        }
+      ));
       if (htmlElem) {
         htmlElem.innerText = data.html;
       }
@@ -93,7 +74,7 @@
         debug: true,
         onChange,
         onReady: onChange,
-        // initData,
+        initData,
       },
     );
   }
