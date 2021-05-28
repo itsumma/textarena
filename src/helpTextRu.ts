c12a3116d23884c1a72dcb8901955368aaf5cda9
exsmund <exsmund@gmail.com>
2021-05-28 09:22:29 +0800

    Hepl popup

---
+++ src/helpTextRu.ts
@@ -0,0 +1,81 @@
+const helpTextRu = `
+<h2>Горячие клавиши</h2>
+<p>
+<table>
+  <tr>
+    <td>Alt + Дефис </td><td> — (длинное тире) </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + Дефис </td><td> – (короткое тире)</td>
+  </tr>
+  <tr>
+    <td>Alt + = </td><td> ≠ </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + = </td><td> ± </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + / </td><td> знак ударе́ния </td>
+  </tr>
+  <tr>
+    <td>Alt + E </td><td> € </td>
+  </tr>
+  <tr>
+    <td>Alt + R </td><td> ® </td>
+  </tr>
+  <tr>
+    <td>Alt + T </td><td> ™ </td>
+  </tr>
+  <tr>
+    <td>Alt + Y </td><td> ѣ </td>
+  </tr>
+  <tr>
+    <td>Alt + A </td><td> ≈ </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + A </td><td> ⌘ </td>
+  </tr>
+  <tr>
+    <td>Alt + S </td><td> § </td>
+  </tr>
+  <tr>
+    <td>Alt + D </td><td> ° </td>
+  </tr>
+  <tr>
+    <td>Alt + F </td><td> £ </td>
+  </tr>
+  <tr>
+    <td>Alt + H </td><td> ₽ </td>
+  </tr>
+  <tr>
+    <td>Alt + X </td><td> × (знак умножения)</td>
+  </tr>
+  <tr>
+    <td>Alt + C </td><td> © </td>
+  </tr>
+  <tr>
+    <td>Alt + M </td><td> − (знак минуса) </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + M </td><td> • </td>
+  </tr>
+  <tr>
+    <td>Alt + < </td><td> « (открывающая кавычка «ёлочка») </td>
+  </tr>
+  <tr>
+    <td>Alt + > </td><td> » (закрывающая кавычка «ёлочка») </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + < </td><td> „ (открывающая кавычка «лапки», обычно для вложенных кавычек) </td>
+  </tr>
+  <tr>
+    <td>Alt + Shift + > </td><td> “ (закрывающая кавычка «лапки», обычно для вложенных кавычек)</td>
+  </tr>
+  <tr>
+    <td>Ctrl + Пробел </td><td> неразрывный пробел</td>
+  </tr>
+</table>
+</p>
+`;
+
+export default helpTextRu;
--- src/services/SymbolCounter.ts
+++ src/services/SymbolCounter.ts
@@ -2,6 +2,7 @@ import Typograf from 'typograf';
 import ArenaServiceManager from './ArenaServiceManager';
 import ElementHelper from '../helpers/ElementHelper';
 import { AnyArenaNode } from '../interfaces/ArenaNode';
+import helpTextRu from '../helpTextRu';
 
 function debounce<T extends Array<unknown>>(
   func: (...args: T) => void,
@@ -28,21 +29,32 @@ function debounce<T extends Array<unknown>>(
 export default class SymbolCounter {
   elem: ElementHelper;
 
+  container: ElementHelper;
+
   content: ElementHelper;
+
+  helpWrap: ElementHelper | undefined;
+
+  lastDocumentOverflow = '';
 
   constructor(
     private asm: ArenaServiceManager,
   ) {
     this.elem = new ElementHelper('ARENA-COUNTER', 'textarena-counter');
     this.content = new ElementHelper('DIV', 'textarena-counter__content');
-    const button = new ElementHelper('BUTTON', 'textarena-typograf', 'Типограф <sup>beta</sup>');
-    button.onClick(() => {
+    const typoButton = new ElementHelper('BUTTON', 'textarena-typograf', 'Типограф <sup>beta</sup>');
+    typoButton.onClick(() => {
       this.typograf();
     });
+    const helpButton = new ElementHelper('BUTTON', 'textarena-help', '?');
+    helpButton.onClick(() => {
+      this.openHelp();
+    });
     this.elem.appendChild(this.content);
-    this.elem.appendChild(button);
-    const container = this.asm.textarena.getContainerElement();
-    container.appendChild(this.elem);
+    this.elem.appendChild(typoButton);
+    this.elem.appendChild(helpButton);
+    this.container = this.asm.textarena.getContainerElement();
+    this.container.appendChild(this.elem);
     this.asm.eventManager.subscribe('ready', this.update.bind(this));
     this.asm.eventManager.subscribe('modelChanged', debounce(this.update.bind(this), 1000));
   }
@@ -66,6 +78,7 @@ export default class SymbolCounter {
     const tp = new Typograf({ locale: ['ru', 'en-US'] });
     this.asm.model.runOfChildren(this.asm.model.model, (n: AnyArenaNode) => {
       if (n.hasText) {
+        n.clearSpaces();
         n.setRawText(tp.execute(n.getRawText()));
       }
     });
@@ -74,5 +87,39 @@ export default class SymbolCounter {
       this.asm.history.save(sel);
     }
     this.asm.eventManager.fire('modelChanged', sel);
+  }
+
+  public openHelp(): void {
+    const wrap = new ElementHelper('DIV', 'textarena-help-wrap');
+    wrap.onClick(() => {
+      this.closeHelp();
+    });
+    const close = new ElementHelper('BUTTON', 'textarena-help-close', `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 512.001 512.001" xml:space="preserve">
+    <g>
+      <path d="M294.111,256.001L504.109,46.003c10.523-10.524,10.523-27.586,0-38.109c-10.524-10.524-27.587-10.524-38.11,0L256,217.892
+        L46.002,7.894c-10.524-10.524-27.586-10.524-38.109,0s-10.524,27.586,0,38.109l209.998,209.998L7.893,465.999
+        c-10.524,10.524-10.524,27.586,0,38.109c10.524,10.524,27.586,10.523,38.109,0L256,294.11l209.997,209.998
+        c10.524,10.524,27.587,10.523,38.11,0c10.523-10.524,10.523-27.586,0-38.109L294.111,256.001z" fill="currentColor"/>
+    </g>
+    </svg>`);
+    close.onClick(() => {
+      this.closeHelp();
+    });
+    const help = new ElementHelper('DIV', 'textarena-help-popup', helpTextRu);
+    help.appendChild(close);
+    wrap.appendChild(help);
+    this.lastDocumentOverflow = document.body.style.overflow;
+    document.body.style.overflow = 'hidden';
+    this.container.appendChild(wrap);
+    this.helpWrap = wrap;
+  }
+
+  public closeHelp(): void {
+    if (this.helpWrap) {
+      this.helpWrap.remove();
+      this.helpWrap = undefined;
+    }
+    document.body.style.overflow = this.lastDocumentOverflow;
   }
 }
