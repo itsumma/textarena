3b9bff3e9d0e7fbfe0d948e0e0b19cc59355254f
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2021-11-09 19:15:06 +0800

    feat: 🔗 Link hotkey test

---
+++ cypress/integration/link.spec.ts
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+import { isMac } from '../../src/utils/navigator';
+
+const ctrl = isMac() ? 'cmd' : 'ctrl';
+
+const url = 'https://github.com/itsumma/textarena';
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/');
+    cy.get('.textarena-editor').focus();
+    cy.get('.textarena-editor').as('root');
+
+    cy.fixture('example.json').as('example');
+  });
+  it('Keyboard shortcut link', () => {
+    cy.get('@root')
+      .focus()
+      .type('{selectall}')
+      .type('{del}')
+      .type('Textarena')
+      .type('{selectall}')
+      .type(`{${ctrl}+k}`)
+      .get('input#url', { includeShadowDom: true })
+      .type(`${url}{enter}`, { force: true, waitForAnimations: false })
+      .get('#html')
+      .contains(`<a href="${url}" target="_blank">Textarena</a>`);
+  });
+});
