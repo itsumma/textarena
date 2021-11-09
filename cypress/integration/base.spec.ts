dd4d2312e297cb23ea36c808ad9ad8b883b40650
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2021-11-09 15:00:21 +0800

    feat: 🧱 Block moving tests

--- cypress/integration/base.spec.ts
+++ cypress/integration/base.spec.ts
@@ -170,4 +170,25 @@ context('Actions', () => {
         .contains('<p class="paragraph">Two line text.</p>');
     });
   });
+
+  it('Block moving', () => {
+    cy.get('@root')
+      .focus()
+      .type('{selectall}')
+      .type('{del}')
+      .type('Line 1')
+      .type('{enter}')
+      .type('Line 2')
+      .type('{enter}')
+      .type('Line 3')
+      .type(`{shift+${isMac() ? 'ctrl' : 'alt'}+uparrow}`)
+      .type('{uparrow}')
+      .type(`{shift+${isMac() ? 'ctrl' : 'alt'}+downarrow}`)
+      .type(`{shift+${isMac() ? 'ctrl' : 'alt'}+downarrow}`);
+    cy.wait(100);
+    cy.get('#html')
+      .contains(
+        '<p class="paragraph">Line 3</p><p class="paragraph">Line 2</p><p class="paragraph">Line 1</p>',
+      );
+  });
 });
