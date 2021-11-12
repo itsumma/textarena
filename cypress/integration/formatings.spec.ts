8d45fdd6c4fda259f3549da906b909af375d14a4
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2021-11-12 16:05:12 +0800

    feat: ✍️ Add formatings tests

--- cypress/integration/formatings.spec.ts
+++ cypress/integration/formatings.spec.ts
@@ -20,14 +20,33 @@ context('Actions', () => {
       .type('{del}')
       .type('Normal ')
       .type(`{${ctrl}+b}`)
-      .type('bold ')
-      .type(`{${ctrl}+b}`)
+      .type('bold')
+      .type(`{${ctrl}+b} `)
       .type(`{${ctrl}+i}`)
-      .type('italic ')
-      .type(`{${ctrl}+i}`)
-      .type('normal2.');
-    cy.wait(10);
-    cy.get('#html')
-      .contains('<p class="paragraph">Normal <strong>bold </strong><em>italic </em>normal2.</p>');
+      .type('italic')
+      .type(`{${ctrl}+i} `)
+      .type(`{${ctrl}+u}`)
+      .type('underlined')
+      .type(`{${ctrl}+u} `)
+      .type(`{${ctrl}+d}`)
+      .type('strikethrough')
+      .type(`{${ctrl}+d} `)
+      .type(`{${ctrl}+,}`)
+      .type('sub')
+      .type(`{${ctrl}+,} `)
+      .type(`{${ctrl}+p}`)
+      .type('sup')
+      .type(`{${ctrl}+p} `)
+      .type(`{${ctrl}+h}`)
+      .type('marked')
+      .type(`{${ctrl}+h} `)
+      .type(`{${ctrl}+e}`)
+      .type('code')
+      .type(`{${ctrl}+e} `)
+      .type('normal2.')
+      .get('#html')
+      .contains(
+        '<p class="paragraph">Normal <strong>bold</strong> <em>italic</em> <u>underlined</u> <s>strikethrough</s> <sub>sub</sub> <sup>sup</sup> <mark>marked</mark> <code>code</code> normal2.</p>',
+      );
   });
 });
